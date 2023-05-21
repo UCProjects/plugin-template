85a9831253f9918a59b1cc68f153bc619f0d109e
Alan H <879790+feildmaster@users.noreply.github.com>
2023-05-21 16:46:24 -0500

    chore: use underscript checker

--- src/index.js
+++ src/index.js
@@ -1,50 +1,9 @@
 const PLUGIN_NAME = '';
+checkUnderscript(PLUGIN_NAME);
+
 const underscript = window.underscript;
+const plugin = underscript.plugin(PLUGIN_NAME, GM_info.version);
 
-function setup() {
-  const plugin = underscript.plugin(PLUGIN_NAME, GM_info.version);
-
-  plugin.events.on(':loaded', () => {
-    // Do stuff
-  });
-}
-
-function warn() {
-  console.error(`${PLUGIN_NAME}: UnderScript required`);
-
-  const key = 'underscript.required';
-  if (sessionStorage.getItem(key)) return;
-  const message = "Looks like you don't have UnderScript installed, or you deactivated it! In order for plugins to work, you need to have it up and running. Until then, the features of this userscript will simply not work. Thank you for your understanding.";
-
-  let alerted = true;
-  if (window.SimpleToast) {
-    SimpleToast({
-      title: 'Missing Requirements',
-      text: message,
-      footer: PLUGIN_NAME,
-    });
-  } else if (window.BootstrapDialog) {
-    BootstrapDialog.show({
-      title: 'Oh No!',
-      type: BootstrapDialog.TYPE_WARNING,
-      message,
-      buttons: [{
-        label: 'Proceed',
-        cssClass: 'btn-primary',
-        action(dialog) {
-          dialog.close();
-        },
-      }],
-    });
-  } else {
-    alerted = false;
-  }
-
-  if (alerted) sessionStorage.setItem(key, '1');
-}
-
-if (underscript) {
-  setup();
-} else {
-  warn();
-}
+plugin.events.on(':loaded', () => {
+  // Do stuff
+});
