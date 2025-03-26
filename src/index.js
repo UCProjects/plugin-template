c4a44f97efa3c2a65bc1471f43331b2fc889e2e9
Alan H <879790+feildmaster@users.noreply.github.com>
2025-03-25 23:44:39 -0500

    Update to latest underscript standards

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-const PLUGIN_NAME = '';
-checkUnderscript(PLUGIN_NAME);
+const PLUGIN_NAME = GM_info.name;
 
 const underscript = window.underscript;
 const plugin = underscript.plugin(PLUGIN_NAME, GM_info.version);
 
-plugin.events.on(':loaded', () => {
+plugin.events.on(':preload', () => {
   // Do stuff
 });
